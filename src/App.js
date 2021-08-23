7c4bfcba7e1635702b5da2575358db3933bff6e0
Luis Ramirez <sdaewa22@gmail.com>
2021-08-23 20:50:02 +0200

    header/footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-function App() {
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+    marginTop: "auto",
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer">
-      Learn React
-    </a>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            File compressor
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>Compressor here</div>
+      </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+      </footer>
+    </>
   );
-}
+};
 
 export default App;
