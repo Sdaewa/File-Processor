435e14adaa639419e90d1f50abca55f64d4a7531
Luis Ramirez <sdaewa22@gmail.com>
2021-08-30 12:37:36 +0200

    redone fileupload component

--- src/Components/FileUpload.jsx
+++ src/Components/FileUpload.jsx
@@ -1,121 +1,29 @@
-import React, { useRef, useState } from "react";
-import {
-  FileUploadContainer,
-  FormField,
-  DragDropText,
-  UploadFileBtn,
-  FilePreviewContainer,
-  ImagePreview,
-  PreviewContainer,
-  PreviewList,
-  FileMetaData,
-  RemoveFileIcon,
-  InputLabel,
-} from "./fileUpStyles";
+import React from "react";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
 
-const DEFAULT_MAX_FILE_SIZE_IN_BYTES = 500000;
-const KILO_BYTES_PER_BYTE = 1000;
+import { DropzoneArea } from "material-ui-dropzone";
 
-const convertBytesToKB = (bytes) => Math.round(bytes / KILO_BYTES_PER_BYTE);
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    previewChip: {
+      minWidth: 160,
+      maxWidth: 210,
+    },
+  })
+);
 
-const FileUpload = ({
-  label,
-  updateFilesCb,
-  maxFileSizeInBytes = DEFAULT_MAX_FILE_SIZE_IN_BYTES,
-  ...otherProps
-}) => {
-  const fileInputField = useRef(null);
-  const [files, setFiles] = useState({});
-
-  const handleUploadBtn = () => {
-    fileInputField.current.click();
-  };
-
-  const addNewFiles = (newFiles) => {
-    for (let file of newFiles) {
-      if (file.size <= maxFileSizeInBytes) {
-        if (!otherProps.multiple) {
-          return { file };
-        }
-        files[file.name] = file;
-      }
-    }
-    return { ...files };
-  };
-
-  const convertNestedObjectToArray = (nestedObj) =>
-    Object.keys(nestedObj).map((key) => nestedObj[key]);
-
-  const callUpdateFilesCb = (files) => {
-    const filesAsArray = convertNestedObjectToArray(files);
-    updateFilesCb(filesAsArray);
-  };
-
-  const removeFile = (fileName) => {
-    delete files[fileName];
-    setFiles({ ...files });
-    callUpdateFilesCb({ ...files });
-  };
-
-  const handleNewUpload = (e) => {
-    const { files: newFiles } = e.target;
-    if (newFiles.length) {
-      let updatedFiles = addNewFiles(newFiles);
-      setFiles(updatedFiles);
-      callUpdateFilesCb(updatedFiles);
-    }
-  };
+const FileUpload = () => {
+  const classes = useStyles();
 
   return (
-    <>
-      <FileUploadContainer>
-        <InputLabel>{label}</InputLabel>
-        <DragDropText>Drag and drop your files anywhere or</DragDropText>
-        <UploadFileBtn type="button" onClick={handleUploadBtn}>
-          <i className="fas fa-file-upload" />
-          <span> Upload {otherProps.multiple ? "files" : "a file"}</span>
-        </UploadFileBtn>
-        <FormField
-          type="file"
-          ref={fileInputField}
-          onChange={handleNewUpload}
-          title=""
-          value=""
-          {...otherProps}
-        />
-      </FileUploadContainer>
-      <FilePreviewContainer>
-        <span>To Upload</span>
-        <PreviewList>
-          {Object.keys(files).map((fileName, index) => {
-            let file = files[fileName];
-            console.log(file);
-
-            return (
-              <PreviewContainer key={fileName}>
-                <div>
-                  <ImagePreview
-                    src={URL.createObjectURL(file)}
-                    alt={`file preview ${index}`}
-                  />
-
-                  <FileMetaData>
-                    <span>{file.name}</span>
-                    <aside>
-                      <span>{convertBytesToKB(file.size)} kb</span>
-                      <RemoveFileIcon
-                        className="fas fa-trash-alt"
-                        onClick={() => removeFile(fileName)}
-                      />
-                    </aside>
-                  </FileMetaData>
-                </div>
-              </PreviewContainer>
-            );
-          })}
-        </PreviewList>
-      </FilePreviewContainer>
-    </>
+    <DropzoneArea
+      showPreviews={true}
+      showPreviewsInDropzone={false}
+      useChipsForPreview
+      previewGridProps={{ container: { spacing: 1, direction: "row" } }}
+      previewChipProps={{ classes: { root: classes.previewChip } }}
+      previewText="Selected files"
+    />
   );
 };
 
