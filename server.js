91436d825981b1dd585414fa4fc36d8749a2ad65
Luis Ramirez <sdaewa22@gmail.com>
2021-08-31 14:04:40 +0200

    multer

--- server.js
+++ server.js
@@ -5,5 +5,18 @@ const cors = require("cors");
 const app = express();
 const port = 8000;
 
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage }).single("file");
+
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port port!`));
